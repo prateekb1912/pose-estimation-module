ab0d192e29777c87723bb601cbf392ce2223261d
prateek-ml <prateekb1912@gmail.com>
2021-07-04 10:30:13 +0530

    Add argument parser to let the user choose their own videos

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 import cv2
+import argparse
 from PoseModule import PoseEstimator
 
+# Add command-line arguments parser
+parser = argparse.ArgumentParser(description="Use the pose estimator module to use in your projects")
+parser.add_argument('--video', '-V', type=str, nargs=1, help="Input video to draw estimated poses on")
+
+args = parser.parse_args()
+
+def main():
+    # Initialize video capture object
+    cap = cv2.VideoCapture(args['video'])
+
+    detector = PoseEstimator()
+    i = 0 
+    
+    # Start reading from the video file
+    while cv2.waitKey(1) != 27:
+        succ, img = cap.read()
+        img = detector.findPose(img)
+        
+        cv2.imshow("Video", img)
+
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
