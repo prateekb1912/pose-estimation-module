0e131050bb92766dfde7f501067bb921c4c8f0e7
prateek-ml <prateekb1912@gmail.com>
2021-07-04 09:39:49 +0530

    Add pose detection example images

--- pose_estimation_min.py
+++ pose_estimation_min.py
@@ -12,10 +12,13 @@ mpDraw = mp.solutions.drawing_utils
 cap = cv2.VideoCapture('smith.mp4')
 
 prev = 0
+i = 0 
 # Start reading from the video file
-while cv2.waitKey(1)!= 27:
+while True:
     succ, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    img_name = f"saved_images/smith{i}.jpg"
 
     # Pass our video frames as images to the model object
     results = pose.process(imgRGB)
@@ -30,6 +33,13 @@ while cv2.waitKey(1)!= 27:
 
     cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 3, (0, 255, 255),2)
 
-    print(results.pose_landmarks)
-
     cv2.imshow("Video", img)
+
+    if cv2.waitKey(1) == ord('s'):    
+        cv2.imwrite(img_name, img)
+        i += 1
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
