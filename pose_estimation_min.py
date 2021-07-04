4054075c613aac64c2d56beb98b84e0ead24856f
prateek-ml <prateekb1912@gmail.com>
2021-07-04 09:12:32 +0530

    Calculate FPS for the original video

--- pose_estimation_min.py
+++ pose_estimation_min.py
@@ -1,12 +1,21 @@
 import cv2
 import numpy as np
 import mediapipe as mp
+import time
 
 # Initialize video capture object
 cap = cv2.VideoCapture('Footwork.mp4')
 
+prev = 0
 # Start reading from the video file
-while True:
+while cv2.waitKey(24)!= 27:
     succ, img = cap.read()
+
+    # Calculating FPS
+    curr = time.time()
+    fps = 1/(curr-prev)
+    prev = curr
+
+    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 3, (0, 255, 255),2)
+
     cv2.imshow("Video", img)
-    cv2.waitKey(1)
